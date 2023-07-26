3a6a8073902dddd4f327715178798589e608b3e9
Semyon Savchenko <savhenko10@mail.ru>
2023-07-26 15:24:37 +0700

    Add report

---
+++ src/main/java/org/test/custom/plugin/jira/reports/ReportEmployee.java
@@ -0,0 +1,12 @@
+package org.test.custom.plugin.jira.reports;
+
+import com.atlassian.jira.plugin.report.impl.AbstractReport;
+import com.atlassian.jira.web.action.ProjectActionSupport;
+
+import java.util.Map;
+
+public class ReportEmployee extends AbstractReport {
+    public String generateReportHtml(ProjectActionSupport projectActionSupport, Map map) throws Exception {
+        return null;
+    }
+}
--- src/main/java/org/test/custom/plugin/jira/webwork/PluginAction.java
+++ src/main/java/org/test/custom/plugin/jira/webwork/PluginAction.java
@@ -1,23 +1,33 @@
 package org.test.custom.plugin.jira.webwork;
 
-import com.atlassian.sal.api.pluginsettings.PluginSettings;
-import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
-import com.atlassian.velocity.VelocityManager;
+import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.user.UserUtils;
+import com.atlassian.jira.web.action.JiraWebActionSupport;
+import com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import com.atlassian.jira.web.action.JiraWebActionSupport;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 public class PluginAction extends JiraWebActionSupport
 {
     private static final Logger log = LoggerFactory.getLogger(PluginAction.class);
     private String param;
-
+    private Set<ApplicationUser> users;
     @Override
     public String execute() {
+        this.users = Sets.newHashSet(UserUtils.getAllUsers());
         this.param = "hello";
+
         return SUCCESS;
+    }
+
+    public Set<ApplicationUser> getUsers(){
+        return users;
+    }
+
+    public void setUsers(Set<ApplicationUser> users) {
+        this.users = users;
     }
 
     public String getParam(){
